00aa14e70c4258a2e52558ea9ca54666272c82ad
last98 <last6351@colorado.edu>
2019-03-22 18:37:23 -0600

    Homework 2
    Javascript, jQuery

---
+++ Question 3-JQuery/scripts.js
@@ -0,0 +1,28 @@
+function calC(c) {  
+    // TODO: 3.1
+    // This function does the evaluation for the calculator
+    /*
+    How this function would look if it were in Javascript would be:
+    document.getElementById('panel').value += c;
+    You need to convert the line above to JQuery.
+    If you are writing more than a couple of lines of code, something is off. Check the reference links again
+    */       
+    $("#panel").val(function(index, value) {
+        return value + c;
+    })
+   
+}
+function CE() {
+    // TODO: 3.2
+    // This function performs the clear function
+    /*
+    How this function would look if it were in Javascript would be:
+    document.getElementById('panel').value = "";
+    You need to convert the line above to JQuery.
+    If you are writing more than a couple of lines of code, something is off. Check the reference links again
+    */      
+    $("#panel").val(function(){
+        return "";
+    });
+}
+
